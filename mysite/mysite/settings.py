9f19ac5641261e751e3caa480f8238d28f7d0aba
mohsennasr1989 <mohsennasr1989@gmail.com>
2021-01-28 23:17:57 +0330

    End of lesson

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'product:index'
 LOGOUT_REDIRECT_URL = 'product:index'
 LOGIN_URL = 'login'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -41,4 +41,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html', ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='products/index.html'), name='logout'),
     path('profile/', user_view.profile, name='profile'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
